95ce68e39e2d4acbdbea82e3cf2dc916313adbea
Margo Urey <margo@margonline.co.uk>
2014-06-21 16:44:14 +0100

    set up after_sign_in method to enable redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,20 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  def after_sign_in_path_for(resource)
+    sign_in_url = url_for(:
+      action => 'new',
+      :controller => 'sessions',
+      :only_path => false,
+      :protocol => 'http'
+    )
+    if request.referer == sign_in_url
+      super
+    else
+      stored_location_for(resource) || request.referer || root_path
+    end
+  end
+
   def configure_permitted_params
     devise_parameter_sanitizer.for(:sign_up) do |u|
       u.permit(:first_name, :last_name, :email, :password, :password_confirmation, :mobile_number, :home_phone_number, :relation_to_player)
