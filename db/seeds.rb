f5c8d7e9409809e8521c4d4c718bd02921b62e9d
Margo Urey <margo@margonline.co.uk>
2014-06-24 17:52:54 +0100

    drop database and recreate from seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,11 @@ if Rails.env.development?
   ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis
   pulvinar facilisis."
 
+  short_descrip = "Donec eu libero sit amet quam egestas semper. Aenean ultricies
+  mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien
+  ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae,
+  ornare sit amet, wisi."
+
   bio = "Pellentesque habitant morbi tristique senectus et netus et malesuada
   fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget,
   tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean
@@ -34,6 +39,17 @@ if Rails.env.development?
   camp4 = Camp.create(:name => "Pre Season Training", :description => description, :location_id => location2.id, :num_of_attendees => 40)
   camp5 = Camp.create(:name => "Match Play", :description => description, :location_id => location2.id, :num_of_attendees => 25)
 
+  CampSession.create([
+    {:camp_id => camp1.id, :start_time => Time.new(2014, 8, 1, 10, 0, 0), :end_time => Time.new(2014, 8, 1, 16, 0, 0), :title => "Penalties", :description => short_descrip},
+    {:camp_id => camp1.id, :start_time => Time.new(2014, 8, 2, 10, 0, 0), :end_time => Time.new(2014, 8, 2, 16, 0, 0), :title => "Short Corners", :description => short_descrip},
+    {:camp_id => camp1.id, :start_time => Time.new(2014, 8, 3, 10, 0, 0), :end_time => Time.new(2014, 8, 3, 16, 0, 0), :title => "Long Corners", :description => short_descrip},
+    {:camp_id => camp2.id, :start_time => Time.new(2014, 9, 1, 10, 0, 0), :end_time => Time.new(2014, 9, 1, 16, 0, 0), :title => "Power plays", :description => short_descrip},
+    {:camp_id => camp2.id, :start_time => Time.new(2014, 9, 2, 10, 0, 0), :end_time => Time.new(2014, 9, 2, 16, 0, 0), :title => "Corners", :description => short_descrip},
+    {:camp_id => camp2.id, :start_time => Time.new(2014, 9, 3, 10, 0, 0), :end_time => Time.new(2014, 9, 3, 16, 0, 0), :title => "Breakaways", :description => short_descrip},
+    {:camp_id => camp3.id, :start_time => Time.new(2014, 7, 1, 10, 0, 0), :end_time => Time.new(2014, 7, 1, 16, 0, 0), :title => "Corners", :description => short_descrip},
+    {:camp_id => camp3.id, :start_time => Time.new(2014, 7, 2, 10, 0, 0), :end_time => Time.new(2014, 7, 2, 16, 0, 0), :title => "Breakaways", :description => short_descrip},
+    {:camp_id => camp3.id, :start_time => Time.new(2014, 7, 3, 10, 0, 0), :end_time => Time.new(2014, 7, 3, 16, 0, 0), :title => "Power Plays", :description => short_descrip}
+    ])
   Coach.create([
     {:first_name => "Alex", :last_name => "Ferguson", :bio => bio, :gravatar => ""},
     {:first_name => "Jose", :last_name => "Mourinho", :bio => bio, :gravatar => ""},
