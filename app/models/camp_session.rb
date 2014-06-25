812445a4eca7ea9167483cda6e972469b5c093f5
Margo Urey <margo@margonline.co.uk>
2014-06-25 20:09:41 +0100

    camp_session method to display session with date

--- app/models/camp_session.rb
+++ app/models/camp_session.rb
@@ -7,5 +7,9 @@ class CampSession < ActiveRecord::Base
   validates :end_time, :presence => true
 
   scope :earliest, -> { order("start_time ASC").first }
+  scope :chronological, -> {order("start_time ASC") }
 
+  def title_with_start_date
+    @title_with_start_date = "#{camp.name} - #{start_time.strftime('%A, %d %b %Y')}"
+  end
 end
--- spec/models/camp_session_spec.rb
+++ spec/models/camp_session_spec.rb
@@ -19,4 +19,10 @@ describe CampSession do
     @earlier_cs = FactoryGirl.create(:campSession, :start_time => Time.new(2014,6,30,9,0,0))
     expect(CampSession.earliest).to eq @earlier_cs
   end
+
+  context 'display name' do
+    it 'should include the start date' do
+      expect(@campSession.title_with_start_date).to eq 'Goalkeeping - Friday, 11 Jul 2014'
+    end
+  end
 end
