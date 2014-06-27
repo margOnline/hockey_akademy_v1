eae2c58405f656d11d3f39227df21a1af2cdeb3c
Margo Urey <margo@margonline.co.uk>
2014-06-27 21:01:43 +0100

    remove register method - not needed

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,11 +6,5 @@ class StaticPagesController < ApplicationController
   def coach_profiles
     @coaches = Coach.all
   end
-#TODO remove this method
-  def register
-     @camp_sessions = CampSession.chronological
-     @players = current_parent.players if parent_signed_in?
-    #@camps = Camp.includes(:campSessions).all
-  end
 
 end
