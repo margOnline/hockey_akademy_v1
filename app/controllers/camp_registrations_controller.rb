6310899baebb93e51e9572514ad50b1aa2fac96c
Margo Urey <margo@margonline.co.uk>
2014-06-27 16:35:21 +0100

    change strong params to accept an array for camp_session_id

--- app/controllers/camp_registrations_controller.rb
+++ app/controllers/camp_registrations_controller.rb
@@ -1,6 +1,7 @@
 class CampRegistrationsController < ApplicationController
 
   before_action :set_associations, :only => [:new, :create]
+  before_action
 
   def new
     @camp_registration = CampRegistration.new
@@ -18,19 +19,26 @@ class CampRegistrationsController < ApplicationController
   end
 
   def show
-    @camp_registrations = CampRegistrations.find(
+    @camp_registrations = CampRegistration.find(
       where :camp_session_id => params[:camp_session_id], :player_id => params[:player_id])
   end
 
   private
 
   def camp_registration_params
-    params.require(:camp_registration).permit(:camp_session_id, :player_id)
+    clear_blank_params(params)
+    params.require(:camp_registration).permit({:camp_session_id => []}, :player_id)
   end
 
   def set_associations
     @camp_sessions = CampSession.chronological
     @players = current_parent.players if parent_signed_in?
+  end
+
+  def clear_blank_params(params)
+    params['camp_registration'].each do |k,v|
+      v.reject!(&:blank?) if v.is_a?(Array)
+    end
   end
 
 end
