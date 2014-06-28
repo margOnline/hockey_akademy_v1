e5ba829443f3338ac0b4faa9455421eec54a93bc
Margo Urey <margo@margonline.co.uk>
2014-06-28 22:16:53 +0100

    delete camp_registration and add camp registration total to view

--- app/helpers/camp_registrations_helper.rb
+++ app/helpers/camp_registrations_helper.rb
@@ -1,2 +1,8 @@
 module CampRegistrationsHelper
+
+  def total(camp_registrations)
+    total = camp_registrations.inject(0) {|sum,cr| sum + cr.camp_session.price}
+    number_to_currency(total, :unit => "£")
+  end
+
 end
--- spec/models/camp_registration_spec.rb
+++ spec/models/camp_registration_spec.rb
@@ -7,6 +7,7 @@ describe CampRegistration do
    subject { @camp_registration }
 
   it { should belong_to(:player) }
-  it { should belong_to(:camp_session)}
+  it { should belong_to(:camp_session) }
+  it { should belong_to(:parent) }
   it { should be_valid }
 end
