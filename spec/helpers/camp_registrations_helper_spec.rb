d72c4a12be4d42f3aefa5fa565d934677a4eadac
Margo Urey <margo@margonline.co.uk>
2014-06-30 10:59:13 +0100

    fix test for camp_registrations_helper, method is looking for an array

--- spec/helpers/camp_registrations_helper_spec.rb
+++ spec/helpers/camp_registrations_helper_spec.rb
@@ -3,11 +3,11 @@ require 'spec_helper'
 
 describe CampRegistrationsHelper do
   before do
-    @cr = FactoryGirl.create(:camp_registration)
+    @crs = [FactoryGirl.create(:camp_registration)]
   end
   describe "total" do
     it "returns the total of camp session prices formatted as a currency" do
-      helper.total(@cr).should eq "£30.00"
+      helper.total(@crs).should eq "£30.00"
     end
   end
 end
