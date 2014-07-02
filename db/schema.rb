78178a47163dfdc2f14c585d68064515a32608bf
Margo Urey <margo@margonline.co.uk>
2014-07-02 23:40:58 +0100

    migration to drop basket and basket_item tables

--- db/schema.rb
+++ db/schema.rb
@@ -11,26 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140702181539) do
+ActiveRecord::Schema.define(version: 20140702223304) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "basket_items", force: true do |t|
-    t.integer  "basket_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "basket_items", ["basket_id"], name: "index_basket_items_on_basket_id", using: :btree
-
-  create_table "baskets", force: true do |t|
-    t.integer  "parent_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "baskets", ["parent_id"], name: "index_baskets_on_parent_id", using: :btree
 
   create_table "camp_registrations", force: true do |t|
     t.integer  "camp_session_id"
@@ -38,10 +22,8 @@ ActiveRecord::Schema.define(version: 20140702181539) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "parent_id"
-    t.integer  "basket_id"
   end
 
-  add_index "camp_registrations", ["basket_id"], name: "index_camp_registrations_on_basket_id", using: :btree
   add_index "camp_registrations", ["camp_session_id"], name: "index_camp_registrations_on_camp_session_id", using: :btree
   add_index "camp_registrations", ["parent_id"], name: "index_camp_registrations_on_parent_id", using: :btree
   add_index "camp_registrations", ["player_id"], name: "index_camp_registrations_on_player_id", using: :btree
