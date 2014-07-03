96d1645709971e29d8a94d7925a4659093e7418b
Margo Urey <margo@margonline.co.uk>
2014-07-03 12:46:03 +0100

    for parent scope in camp_registration model

--- app/models/camp_registration.rb
+++ app/models/camp_registration.rb
@@ -1,9 +1,15 @@
 class CampRegistration < ActiveRecord::Base
   belongs_to :player
   belongs_to :camp_session
-  belongs_to :basket
+  belongs_to :parent
 
   validates :player_id, :presence => true
   validates :camp_session_id, :presence => true
+
+  scope :for_parent, ->(parent) { where(:parent => parent) }
+
+  def total_price
+
+  end
 
 end
--- spec/models/camp_registration_spec.rb
+++ spec/models/camp_registration_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+include Devise::TestHelpers
 
 describe CampRegistration do
   before do
@@ -8,5 +9,20 @@ describe CampRegistration do
 
   it { should belong_to(:player) }
   it { should belong_to(:camp_session)}
+  it { should belong_to(:parent)}
   it { should be_valid }
+
+  context 'scopes' do
+    before do
+      @parent1 = FactoryGirl.create(:parent)
+      @parent2 = FactoryGirl.create(:parent)
+      @cr_for_parent1 = FactoryGirl.create(:camp_registration, :parent => @parent1 )
+      @cr_for_parent2 = FactoryGirl.create(:camp_registration, :parent => @parent2 )
+    end
+
+    it 'returns all camp registrations for a specific parent' do
+      expect(CampRegistration.for_parent(@parent1)).to eq [@cr_for_parent1]
+      expect(CampRegistration.for_parent(@parent1)).to_not eq [@cr_for_parent2]
+    end
+  end
 end
