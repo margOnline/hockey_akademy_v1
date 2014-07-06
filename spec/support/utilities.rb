47fd271dd1b64338a7765d566dcd5b06fa773482
Margo Urey <margo@margonline.co.uk>
2014-07-06 21:16:12 +0100

    add application_helper methods to spec/support/utilities file

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,8 +1,7 @@
-def full_title(page_title)
-  base_title = "Hockey Akademy"
-  if page_title.empty?
-    base_title
-  else
-    "#{base_title} | #{page_title}"
-  end
+def format_date(date)
+  date.strftime('%A, %d %b %Y')
+end
+
+def format_time(time)
+  time.strftime("%A, %d %b %Y %l:%M %p")
 end
