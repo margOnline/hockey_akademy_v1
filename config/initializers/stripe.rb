aac1c579476f7191dcc26ec42f44bdce61714f13
Margo Urey <margo@margonline.co.uk>
2014-07-06 20:28:21 +0100

    ncn

--- config/initializers/stripe.rb
+++
@@ -1,6 +0,0 @@
-Rails.configuration.stripe = {
-  :publishable_key => 'pk_test_4KVD1TVf9Uhlsoggm6uQv39h',
-  :secret_key      => 'sk_test_4KVDfL3hcrE1sKaxDoHYlkcz'
-}
-
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
