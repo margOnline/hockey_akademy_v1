0c4a4bf7c7c0d73840e25710b19914dcba133a75
Margo Urey <margo@margonline.co.uk>
2014-07-06 22:35:50 +0100

    put base_title method back into spec/support/utils after mistakenly deleting

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,3 +1,12 @@
+def full_title(page_title)
+  base_title = "Hockey Akademy"
+  if page_title.empty?
+    base_title
+  else
+    "#{base_title} | #{page_title}"
+  end
+end
+
 def format_date(date)
   date.strftime('%A, %d %b %Y')
 end
