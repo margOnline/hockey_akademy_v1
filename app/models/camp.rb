dfd48fb7d00e8356cbad87a0ce04ef426e1fe43d
Margo Urey <margo@margonline.co.uk>
2014-07-07 18:43:02 +0100

    move camp helper methods into camp model

--- app/models/camp.rb
+++ app/models/camp.rb
@@ -6,4 +6,14 @@ class Camp < ActiveRecord::Base
   validates :name, :presence => true
   validates :description, :presence => true
   validates :num_of_attendees, :presence => true
+
+  def start_time
+    camp_sessions.order(:start_time => :asc).first.start_time
+  end
+
+
+  def end_time
+    camp_sessions.order(:end_time => :desc).first.end_time
+  end
+
 end
